f265bb26ea488cf68aaf03d0d2b693fd59e7928e
paripal <86734575+paripalpp@users.noreply.github.com>
2024-02-20 10:36:26 +0900

    fix:Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -78,7 +78,7 @@ int main(){
     float motor2_speed = encoder3.get_speed();
     float motor_target[4];
 
-    pid_param_t hina_rot_gain{0.5, 0.05, 0};
+    pid_param_t hina_rot_gain{0.5, 0.5, 0};
     Mech mech(&can, 2, 20, 1000e3, 1000e3, 1000e3, 15000, -3000, hina_rot_gain,
     PA_1, PA_0, PullUp, true,
     PC_0, PA_15, PB_7, PC_1, PC_2, PC_3, PB_0, -4.4879895051283, 2.2439947525641);
